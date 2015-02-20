98451167ca9fc1091778e3afe979fbb87e28ef37
mshava <sakhile@projectcodex.co>
2015-02-20 14:20:07 +0200

    task 3 done

---
+++ task3.js
@@ -0,0 +1,12 @@
+ 
+        //javascript code goes here at the bottom to ensure the DOM is loaded...
+
+        document.getElementsByTagName('button')[0].addEventListener("click",function(){
+       		document.getElementsByTagName("div")[0].style.backgroundColor="green";
+
+        });
+    document.getElementsByTagName('button')[1].addEventListener("click",function(){
+       		document.getElementsByTagName("div")[0].style.backgroundColor="red";
+
+        });
+    
